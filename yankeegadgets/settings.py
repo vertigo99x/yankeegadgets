57cf92548dbb6e1a5970625f8c1c5bbb37f039a5
vertigo99x <3ples1234@gmail.com>
2023-09-14 18:06:35 +0100

    edit

--- yankeegadgets/settings.py
+++ yankeegadgets/settings.py
@@ -83,11 +83,23 @@ WSGI_APPLICATION = 'yankeegadgets.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ['ENGINE'],
+        'NAME': os.environ['NAME'],
+        'HOST': os.environ['HOST'],
+        'USER': os.environ['USER'],
+        'PASSWORD': os.environ['PASSWORD'],
+        'PORT': os.environ['PORT'],
     }
 }
 
